bc117c4ec33fcd15fd58df23fe0702e1374da7fa
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2022-07-15 10:16:33 +0530

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -5,9 +5,7 @@ import requests
 from .models import BTCAlert
 
 def get_realtime_price():
-    """
-    get_realtime_price: used for getting realtime price of BTC from coingecko API
-    """
+   
     breakpoint()
     response = requests.get(url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false?')
     json_response=response.json()
@@ -19,8 +17,8 @@ def send_alert_email(receivers,price):
     try:
         subject=f"Alert !! Bitcoin Price Reached..."
         message = f""" 
-        Hello User
-        BitCoin Price is reached to {price}
+        Hello 
+        BitCoin Price is reached {price}
         """
         send_mail(subject,message,EMAIL_HOST_USER,[receivers],
         fail_silently= False)
