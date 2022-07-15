f3527b13e85325bd15eda2cf27f03081bb915fd9
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2022-07-15 10:16:00 +0530

    Update task.py

--- core/task.py
+++ core/task.py
@@ -9,28 +9,20 @@ logger = get_task_logger(__name__)
 
 @shared_task(name = "get_realtime_price_tasks")
 def get_realtime_price_tasks():
-    """
-    get_realtime_price_tasks: used for getting real time current price of btc
-    Match the price
-    Send the Alert
-    
-    """
+   
     data=get_realtime_price()
     price=data['current_price']
     name=data['name']
 
-    print(f"Fetching Price...! of {name}")
     logger.info(f"fetched Price of Bitcoin!! is {price}")
     # alerts=Alert.objects.filter(price=price,status='created')
     alerts=get_alert(price)
     if alerts.exists():
-        print(f"{len(alerts)} Alert Matched...!")
         receivers=[alert.user.email for alert in alerts]
         alert=[alert.id for alert in alerts]
 
         if len(receivers)>0:
             send_alert_to_user.delay(receivers,price)
-            print(f"Alert Sent To..! {receivers}")
             is_triggered=trigger(alert)
             print(is_triggered)
 
